0616e342dbb1abe7e8c84df83b874f3b8d95cf1b
Michał Lach <kontakt@michal-lach.pl>
2026-02-01 20:02:40 +0100

    absolute lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional options -- lua/config/options.lua
+-- This file is loaded automatically by LazyVim
+
+vim.opt.number = true -- show absolute line numbers
+vim.opt.relativenumber = false -- disable relative/hybrid numbershere
